07c69a4e3ef78703344ff857e370c8bd9fc689b9
Spencer <spenclark@hotmail.com>
2022-03-14 16:11:11 -0700

    narbar inti

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Comonents/NavBar';
+import Hero from './Comonents/Hero';
+import Sliders from './Comonents/Sliders';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <Hero/>
+      <Sliders />
+      <
     </div>
   );
 }
