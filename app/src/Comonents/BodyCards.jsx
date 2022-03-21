ae8b678b1ef6d6288b1a2329e6df340098ad8c83
Spencer <spenclark@hotmail.com>
2022-03-21 02:31:10 -0700

    body cards on wide

--- app/src/Comonents/BodyCards.jsx
+++ app/src/Comonents/BodyCards.jsx
@@ -5,19 +5,6 @@ import React from 'react'
 // 2 what we do
 // 3 easy and understand
 
-const data = [{
-  title: "How it works",
-  text: ``,
-  photos: ["","",""],
-},{
-  title: "How we help",
-  text: "",
-  photos: ["","",""],
-},{
-  title: "Simple",
-  text: "",
-  photos: ["","",""],
-}]
 
 function transitonPhoto() {
 
@@ -30,29 +17,61 @@ function Card() {
 
 
 function BodyCards() {
+
+
+
   return (
-    <div>
-      <div>
-        {data.map((e) => {
-          return (
-            <div>
-              <div>
-                {e.title}
-              </div>
-              <div>
-                <div>
-                  {e.photos.map((e) => {
-
-                  })}
-                </div>
-                <div>
-
-                </div>
-              </div>
-  
-            </div>
-          )
-        })}
+    <div className='slide__container'>
+     <div className='slide__div '>
+        <div>
+          <img className='slide__img' src="/curated_portfolio/green_lv.jpg" alt="case 1"></img>
+        </div>
+        <div className='slide__content'>
+          <div className='slide__title'>
+            <p>
+              Why use staging services?
+            </p>
+          </div>
+          <div className='slide__text'>
+            <p>
+                      According to industry research staged homes spend 73 per cent less time on the market and sell for 5% to 25% above listing price. <a href="https://blog.remax.ca/do-staged-homes-sell-faster/" style={{color: "#B4182D"}}>See Report.</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='slide__div__inverted '>
+        <div>
+          <img className='slide__img' src="/curated_portfolio/ntr_light.jpg" alt="case 1"></img>
+        </div>
+        <div className='slide__content'>
+          <div className='slide__title'>
+            <p>
+              How it works
+            </p>
+          </div>
+          <div className='slide__text'>
+            <p>
+              Upstage Design transforms property by reorganizing and restructuing spaces by blending the clients inventory and our own. We work hard to make sure that the property is ready for the market.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='slide__div '>
+        <div>
+          <img className='slide__img' src="/curated_portfolio/prpl_brm.jpg" alt="case 1"></img>
+        </div>
+        <div className='slide__content'>
+          <div className='slide__title'>
+            <p>
+              Resourceful and human
+            </p>
+          </div>
+          <div className='slide__text'>
+            <p>
+              Upstage Design has working with all types of clients. We know how to use what is infront of us and be respectful of poeple property and space.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   )
